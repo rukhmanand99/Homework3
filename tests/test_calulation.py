e2fbc6b708fb24882379ee8c8f6c1a452d148bdd
rukhmanand99 <rukhmanandreddy99@gmail.com>
2025-02-10 22:27:49 -0500

    files

--- tests/test_calulation.py
+++ tests/test_calulation.py
@@ -1,10 +1,22 @@
 """
-Unit tests for the Calculation class.
+Unit tests for the Calculations class.
 """
 
+from calculator.calculations import Calculations
 from calculator.calculation import Calculation
 
-def test_calculation():
-    """Test Calculation object creation and result retrieval."""
+def setup_function():
+    """Clear history before each test."""
+    Calculations.clear_history()
+
+def test_add_calculation():
+    """Test adding a calculation to history."""
     calc = Calculation(lambda x, y: x + y, 2, 3)
-    assert calc.get_result() == 5
+    Calculations.add_calculation(calc)
+    assert len(Calculations.get_history()) == 1
+
+def test_clear_history():
+    """Test clearing history."""
+    Calculations.add_calculation(Calculation(lambda x, y: x + y, 2, 3))
+    Calculations.clear_history()
+    assert len(Calculations.get_history()) == 0
